6e00e6990546e631ce0b6042c2dd951673bf68ea
Richard Kuo <rkuo2000@gmail.com>
2021-11-28 00:47:05 +0800

    Update Generator.py

--- AirSimE2EDeepLearning/Generator.py
+++ AirSimE2EDeepLearning/Generator.py
@@ -44,8 +44,10 @@ class DriveDataGenerator(image.ImageDataGenerator):
                  vertical_flip,
                  rescale,
                  preprocessing_function,
-                 data_format)
+                 data_format,
+                 brightness_range)
         self.brighten_range = brighten_range
+        self.brightness_range = (0.0, 0.0)
 
     def flow(self, x_images, x_prev_states = None, y=None, batch_size=32, shuffle=True, seed=None,
              save_to_dir=None, save_prefix='', save_format='png', zero_drop_percentage=0.5, roi=None):
