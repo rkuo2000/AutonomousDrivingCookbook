f920e5505c0080ef97489e541d6b7f623cf7a0b9
Richard Kuo <rkuo2000@gmail.com>
2021-11-28 03:14:28 +0800

    Update Generator.py

--- AirSimE2EDeepLearning/Generator.py
+++ AirSimE2EDeepLearning/Generator.py
@@ -24,8 +24,7 @@ class DriveDataGenerator(image.ImageDataGenerator):
                  vertical_flip=False,
                  rescale=None,
                  preprocessing_function=None,
-                 data_format=None,
-                 brightness_range=None):
+                 data_format=None):
         super(DriveDataGenerator, self).__init__(featurewise_center,
                  samplewise_center,
                  featurewise_std_normalization,
@@ -44,9 +43,8 @@ class DriveDataGenerator(image.ImageDataGenerator):
                  vertical_flip,
                  rescale,
                  preprocessing_function,
-                 data_format,
-                 brightness_range)        
-        self.brightness_range = brightness_range
+                 data_format)        
+        
     def flow(self, x_images, x_prev_states = None, y=None, batch_size=32, shuffle=True, seed=None,
              save_to_dir=None, save_prefix='', save_format='png', zero_drop_percentage=0.5, roi=None):
         return DriveIterator(
@@ -147,7 +145,7 @@ class DriveDataGenerator(image.ImageDataGenerator):
             if np.random.random() < 0.5:
                 x = image.flip_axis(x, img_row_axis)
                 
-        brighten_range = self.brightness_range([1])
+        brighten_range = 0.4
         if brighten_range != 0:
             random_bright = np.random.uniform(low = 1.0-brighten_range, high=1.0+brighten_range)
             
